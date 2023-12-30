8432eed504f70f9a58fdb7b97c54a815ec67fe8e
hanh <thanhctag1999@gmail.com>
2023-12-30 12:03:24 +0700

    responsive calendar

--- src/views/rooms/components/HorizontalCalendar.js
+++ src/views/rooms/components/HorizontalCalendar.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import DatePicker from 'react-horizontal-datepicker';
+import React, { useState } from 'react';
+import { Datepicker } from '@meinefinsternis/react-horizontal-date-picker';
+import { enUS } from 'date-fns/locale';
 
-export default function HorizontalCalendar() {
-  const selectedDay = (val) => {
-    console.log(val);
+const HorizontalCalendar = () => {
+  const [date, setDate] = useState({
+    startValue: null,
+    endValue: null,
+    rangeDates: [],
+  });
+
+  const handleChange = (d) => {
+    const [startValue, endValue, rangeDates] = d;
+    setDate((prev) => ({ ...prev, endValue, startValue, rangeDates }));
   };
 
   return (
-    <div>
-      <DatePicker getSelectedDay={selectedDay} labelFormat={'MMMM'} color={'#5D87FF'} />
+    <div className="calendar">
+      <Datepicker
+        onChange={handleChange}
+        locale={enUS}
+        startValue={date.startValue}
+        endValue={date.endValue}
+      />
     </div>
   );
-}
+};
+
+export default HorizontalCalendar;
